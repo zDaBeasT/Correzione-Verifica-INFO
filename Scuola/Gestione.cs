45cb49fb103381c5dbe7e24fd7cbe8495b5da9cc
Daniele Hanelli <hanellidaniele@gmail.com>
2021-01-25 19:52:53 +0100

    Add project files.

---
+++ Scuola/Gestione.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Scuola
+{
+    public class Gestione
+    {
+        public static string Inserisci()
+        {
+            string n;
+            Console.WriteLine("inserisci una classe:");
+            n = Convert.ToString(Console.ReadLine());
+            return n;
+        }
+        public static int Risposta()
+        {
+            int risposta;
+            Console.WriteLine("inserire 1 per continuare oppure inserire 0 per uscire:");
+                risposta = int.Parse(Console.ReadLine());
+            return risposta;
+        }
+        public static int Voto()
+        {
+            Console.WriteLine("Inserisci voto scolastico:");
+            int voto = int.Parse(Console.ReadLine());
+            return voto;
+        }
+        public static int Contatore(int cont2) 
+        {
+            int volte = cont2 * 15;
+            return volte;
+        }
+        public static int MediaIstituto(int voto,int cont2 )
+        {
+            
+            int media = voto / (cont2 * 15);
+            return media;
+        }
+        public static int Media(int voto)
+        {
+            int mediaIstituto = voto / 15;
+                return mediaIstituto;
+        }
+    }
+}
---
+++ ScuolaStatale/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Scuola;
+
+namespace ScuolaStatale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cont2 = 0;
+            int risposta = Gestione.Risposta();
+            while (risposta == 1)
+            {
+                string classe = Gestione.Inserisci();
+                risposta = Gestione.Risposta();
+                cont2++;
+            }
+            int volte = Gestione.Contatore(cont2);
+            for (int cont=0; cont < volte; cont++)
+                {
+                    int voto = Gestione.Voto();
+                    if (voto <= 10 && voto >= 2)
+                    {
+                        Console.WriteLine("il voto è stato inserito corettamente.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("voto inserito non valido riprova con un voto da 2 a 10.");
+                    cont--;
+                    }
+                int vototot =+ voto;
+                int media = Gestione.Media(voto);
+                int mediaIstituto = Gestione.MediaIstituto(voto, cont2);
+                Console.WriteLine($"la media dell'istituto è {mediaIstituto}, la media della classe è {media}");
+                Console.ReadLine();
+            }        
+        }
+    }
+}
